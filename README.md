011cc83ec047793cdc76d8b4addb42863ef0b26d
unknown <Vincent@DESKTOP-CSATI6S.kuikdelivery.com>
2020-09-19 00:46:18 -0400

    final version 1.0

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Smokey-Bear-Fodder
+
+Steps to run the app.
+
+1. Run MongoDB
+2. Run python insertData.py
+3. Run python api.py and keep open in background
+4. Run the command 'python -m http.server' from within the fireapp folder or the images will not load
+5. Open browser and visit http://localhost:8000/
+6. Click firemaps.html
--- fireapp/js/buttons.js
+++ fireapp/js/buttons.js
@@ -1,65 +1,98 @@
 var button = d3.select(".CT");
 button.on("click", function() {
+  d3.selectAll(".not-hidden2").attr("class","hidden leaflet-container leaflet-touch leaflet-fade-anim leaflet-grab leaflet-touch-drag leaflet-touch-zoom");
   d3.selectAll(".not-hidden").attr("class","hidden leaflet-container leaflet-touch leaflet-fade-anim leaflet-grab leaflet-touch-drag leaflet-touch-zoom");
   d3.select("#map-ct").attr("class","not-hidden leaflet-container leaflet-touch leaflet-fade-anim leaflet-grab leaflet-touch-drag leaflet-touch-zoom");
+  d3.select(".not-hidden-text").attr("class","hidden-text")
+  d3.select("#text-ct").attr("class","not-hidden-text")
 });
 
 var button2 = d3.select(".DE");
 button2.on("click", function() {
+    d3.selectAll(".not-hidden2").attr("class","hidden leaflet-container leaflet-touch leaflet-fade-anim leaflet-grab leaflet-touch-drag leaflet-touch-zoom");
     d3.selectAll(".not-hidden").attr("class","hidden leaflet-container leaflet-touch leaflet-fade-anim leaflet-grab leaflet-touch-drag leaflet-touch-zoom");
     d3.select("#map-de").attr("class","not-hidden leaflet-container leaflet-touch leaflet-fade-anim leaflet-grab leaflet-touch-drag leaflet-touch-zoom");
+    d3.select(".not-hidden-text").attr("class","hidden-text")
+    d3.select("#text-de").attr("class","not-hidden-text")
 });
 
 var button2 = d3.select(".MA");
 button2.on("click", function() {
+    d3.selectAll(".not-hidden2").attr("class","hidden leaflet-container leaflet-touch leaflet-fade-anim leaflet-grab leaflet-touch-drag leaflet-touch-zoom");
     d3.selectAll(".not-hidden").attr("class","hidden leaflet-container leaflet-touch leaflet-fade-anim leaflet-grab leaflet-touch-drag leaflet-touch-zoom");
     d3.select("#map-ma").attr("class","not-hidden leaflet-container leaflet-touch leaflet-fade-anim leaflet-grab leaflet-touch-drag leaflet-touch-zoom");
+    d3.select(".not-hidden-text").attr("class","hidden-text")
+    d3.select("#text-ma").attr("class","not-hidden-text")
 });
 
 var button2 = d3.select(".MD");
 button2.on("click", function() {
+    d3.selectAll(".not-hidden2").attr("class","hidden leaflet-container leaflet-touch leaflet-fade-anim leaflet-grab leaflet-touch-drag leaflet-touch-zoom");
     d3.selectAll(".not-hidden").attr("class","hidden leaflet-container leaflet-touch leaflet-fade-anim leaflet-grab leaflet-touch-drag leaflet-touch-zoom");
     d3.select("#map-md").attr("class","not-hidden leaflet-container leaflet-touch leaflet-fade-anim leaflet-grab leaflet-touch-drag leaflet-touch-zoom");
+    d3.select(".not-hidden-text").attr("class","hidden-text")
+    d3.select("#text-md").attr("class","not-hidden-text")
 });
 
 var button2 = d3.select(".ME");
 button2.on("click", function() {
+    d3.selectAll(".not-hidden2").attr("class","hidden leaflet-container leaflet-touch leaflet-fade-anim leaflet-grab leaflet-touch-drag leaflet-touch-zoom");
     d3.selectAll(".not-hidden").attr("class","hidden leaflet-container leaflet-touch leaflet-fade-anim leaflet-grab leaflet-touch-drag leaflet-touch-zoom");
     d3.select("#map-me").attr("class","not-hidden leaflet-container leaflet-touch leaflet-fade-anim leaflet-grab leaflet-touch-drag leaflet-touch-zoom");
+    d3.select(".not-hidden-text").attr("class","hidden-text")
+    d3.select("#text-me").attr("class","not-hidden-text")
 });
 
 var button2 = d3.select(".NH");
 button2.on("click", function() {
+    d3.selectAll(".not-hidden2").attr("class","hidden leaflet-container leaflet-touch leaflet-fade-anim leaflet-grab leaflet-touch-drag leaflet-touch-zoom");
     d3.selectAll(".not-hidden").attr("class","hidden leaflet-container leaflet-touch leaflet-fade-anim leaflet-grab leaflet-touch-drag leaflet-touch-zoom");
     d3.select("#map-nh").attr("class","not-hidden leaflet-container leaflet-touch leaflet-fade-anim leaflet-grab leaflet-touch-drag leaflet-touch-zoom");
+    d3.select(".not-hidden-text").attr("class","hidden-text")
+    d3.select("#text-nh").attr("class","not-hidden-text")
 });
 
 var button2 = d3.select(".NJ");
 button2.on("click", function() {
+    d3.selectAll(".not-hidden2").attr("class","hidden leaflet-container leaflet-touch leaflet-fade-anim leaflet-grab leaflet-touch-drag leaflet-touch-zoom");
     d3.selectAll(".not-hidden").attr("class","hidden leaflet-container leaflet-touch leaflet-fade-anim leaflet-grab leaflet-touch-drag leaflet-touch-zoom");
     d3.select("#map-nj").attr("class","not-hidden leaflet-container leaflet-touch leaflet-fade-anim leaflet-grab leaflet-touch-drag leaflet-touch-zoom");
+    d3.select(".not-hidden-text").attr("class","hidden-text")
+    d3.select("#text-nj").attr("class","not-hidden-text")
 });
 
 var button2 = d3.select(".NY");
 button2.on("click", function() {
+    d3.selectAll(".not-hidden2").attr("class","hidden leaflet-container leaflet-touch leaflet-fade-anim leaflet-grab leaflet-touch-drag leaflet-touch-zoom");
     d3.selectAll(".not-hidden").attr("class","hidden leaflet-container leaflet-touch leaflet-fade-anim leaflet-grab leaflet-touch-drag leaflet-touch-zoom");
     d3.select("#map-ny").attr("class","not-hidden leaflet-container leaflet-touch leaflet-fade-anim leaflet-grab leaflet-touch-drag leaflet-touch-zoom");
+    d3.select(".not-hidden-text").attr("class","hidden-text")
+    d3.select("#text-ny").attr("class","not-hidden-text")
 });
 
 var button2 = d3.select(".PA");
 button2.on("click", function() {
+    d3.selectAll(".not-hidden2").attr("class","hidden leaflet-container leaflet-touch leaflet-fade-anim leaflet-grab leaflet-touch-drag leaflet-touch-zoom");
     d3.selectAll(".not-hidden").attr("class","hidden leaflet-container leaflet-touch leaflet-fade-anim leaflet-grab leaflet-touch-drag leaflet-touch-zoom");
     d3.select("#map-pa").attr("class","not-hidden leaflet-container leaflet-touch leaflet-fade-anim leaflet-grab leaflet-touch-drag leaflet-touch-zoom");
+    d3.select(".not-hidden-text").attr("class","hidden-text")
+    d3.select("#text-pa").attr("class","not-hidden-text")
 });
 
 var button2 = d3.select(".RI");
 button2.on("click", function() {
+    d3.selectAll(".not-hidden2").attr("class","hidden leaflet-container leaflet-touch leaflet-fade-anim leaflet-grab leaflet-touch-drag leaflet-touch-zoom");
     d3.selectAll(".not-hidden").attr("class","hidden leaflet-container leaflet-touch leaflet-fade-anim leaflet-grab leaflet-touch-drag leaflet-touch-zoom");
     d3.select("#map-ri").attr("class","not-hidden leaflet-container leaflet-touch leaflet-fade-anim leaflet-grab leaflet-touch-drag leaflet-touch-zoom");
+    d3.select(".not-hidden-text").attr("class","hidden-text")
+    d3.select("#text-ri").attr("class","not-hidden-text")
 });
 
 var button2 = d3.select(".VT");
 button2.on("click", function() {
+    d3.selectAll(".not-hidden2").attr("class","hidden leaflet-container leaflet-touch leaflet-fade-anim leaflet-grab leaflet-touch-drag leaflet-touch-zoom");
     d3.selectAll(".not-hidden").attr("class","hidden leaflet-container leaflet-touch leaflet-fade-anim leaflet-grab leaflet-touch-drag leaflet-touch-zoom");
     d3.select("#map-vt").attr("class","not-hidden leaflet-container leaflet-touch leaflet-fade-anim leaflet-grab leaflet-touch-drag leaflet-touch-zoom");
+    d3.select(".not-hidden-text").attr("class","hidden-text")
+    d3.select("#text-vt").attr("class","not-hidden-text")
 });
